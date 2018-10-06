5eba3e462f6ce4a28c872cffb5d10185517f70df
MalteBartels <malte.bartels@live.de>
2018-10-06 02:30:50 +0200

    Add legal stuff

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.querySelector("#container");
 const text = document.querySelector("p");
 const links = document.querySelectorAll("#links a");
 const message = "Hier gibt es noch nichts zu sehen. Bitte gehen Sie weiter!";
+const impressum_btn = document.querySelector("#impressum_btn");
+const impressum_modal = document.querySelector("#impressum_modal");
 
 container.classList.add("animate-fadein"); 
 
@@ -16,4 +18,12 @@ links.forEach((el, i) => {
   setTimeout(() => 
             links[i].classList.add("animate-slidein"),
              (i*i)*75 + messageRenderTime);
+});
+
+impressum_btn.addEventListener("click", () => {
+    impressum_modal.classList.add("active");
+});
+
+impressum_modal.addEventListener("click", () => {
+    impressum_modal.classList.remove("active");
 });
