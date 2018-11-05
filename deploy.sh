e714bb8557b496dad97474a6800212d8cb285062
MalteBartels <malte.bartels@live.de>
2018-11-05 16:18:01 +0100

    Add vue build to travis

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,10 @@ echo "Starting deploy"
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
     then     echo "Is a pull request, don't deploy!" & exit 0
 fi
-echo "Create delivery folder"
-mkdir -p output/httpdocs
-cp index.html script.js style.css ./output/httpdocs
-cp -r assets/ ./output/httpdocs/assets
+echo "Rename export folder"
+mv dist httpdocs
 echo "Copying to server"
 scp -i deploy_key -r output/httpdocs travis@malte-bartels.de:/var/www/vhosts/malte-bartels.de/
-echo "cleaning up"
-rm -rf output
+echo "cleaning up" # useful for local testing
+rm -rf httpdocs
 echo "Finished deployment"
