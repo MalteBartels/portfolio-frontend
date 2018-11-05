2c07c2b24793faab9fa3252b9a2e148f9b895670
MalteBartels <malte.bartels@live.de>
2018-11-05 16:01:41 +0100

    Add pull request recognition to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 echo "Starting deploy"
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
+    then     echo "Is a pull request, don't deploy!" & exit 0
+fi
 echo "Create delivery folder"
 mkdir -p output/httpdocs
 cp index.html script.js style.css ./output/httpdocs
