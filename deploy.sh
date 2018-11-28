d57876a2ffee47e4e07516c77fa4a9f1446db826
MalteBartels <malte.bartels@live.de>
2018-11-28 10:17:05 +0100

    Improve pull request recognition

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 echo "Starting deploy"
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]
-    then     echo "Is a pull request, don't deploy!" & exit 0
-fi
 echo "Rename export folder"
 mv dist httpdocs
 echo "Copying to server"
-scp -i deploy_key -r output/httpdocs travis@malte-bartels.de:/var/www/vhosts/malte-bartels.de/
+scp -i deploy_key -r httpdocs travis@malte-bartels.de:/var/www/vhosts/malte-bartels.de/
 echo "cleaning up" # useful for local testing
 rm -rf httpdocs
 echo "Finished deployment"
